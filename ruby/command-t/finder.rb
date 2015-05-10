e19a84ba53d2db24a1c058ce42d220581c47a63d
Pankaj Gupta <pankajg@twitter.com>
2015-05-10 02:37:31 -0700

    Use local path matcher instead of calling c stuff.

--- ruby/command-t/finder.rb
+++ ruby/command-t/finder.rb
@@ -2,6 +2,8 @@
 # Licensed under the terms of the BSD 2-clause license.
 
 require 'command-t/ext' # CommandT::Matcher
+require 'net/http'
+require 'uri'
 
 module CommandT
   # Encapsulates a Scanner instance (which builds up a list of available files
@@ -20,7 +22,10 @@ module CommandT
     # Options:
     #   :limit (integer): limit the number of returned matches
     def sorted_matches_for(str, options = {})
-      @matcher.sorted_matches_for str, options
+      #@matcher.sorted_matches_for str, options
+      uri = URI.parse("http://localhost:10120/#{str}")
+      lines = Net::HTTP.get_response(uri).body
+      lines.split("\n")
     end
 
     def open_selection(command, selection, options = {})
